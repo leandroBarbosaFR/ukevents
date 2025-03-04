2d50cf4c2b807b72d2060f8879c586ab017a04f6
Leandro <leandrobarbosa@sfr.fr>
2025-03-04 17:14:22 +0100

    Add files via upload
    start project

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import reactLogo from "./assets/react.png";
+import "./App.css";
+import emailjs from "emailjs-com";
+
+function App() {
+  const [email, setEmail] = useState("");
+
+  const handleEmailChange = (event: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    setEmail(event.target.value);
+  };
+
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault(); // Prevent default form reload
+
+    if (email) {
+      const templateParams = { email };
+
+      emailjs
+        .send(
+          "service_bqvx0ii", // Replace with your EmailJS service ID
+          "template_hmzdn1h", // Replace with your EmailJS template ID
+          templateParams,
+          "NsMvQnaxN8Ow5yJhE" // Replace with your EmailJS public key
+        )
+        .then(
+          (response) => {
+            console.log("Email sent successfully!", response);
+            alert("Thank you for signing up! We will contact you soon.");
+            setEmail(""); // Clear input field
+          },
+          (error) => {
+            console.log("Email send error:", error);
+            alert("There was an error. Please try again later.");
+          }
+        );
+    } else {
+      alert("Please enter a valid email.");
+    }
+  };
+
+  return (
+    <div className="page-container">
+      <img src={reactLogo} className="logo" alt="React logo" />
+      <h1 className="title">Coming Soon</h1>
+
+      <div className="form-wrapper">
+        <form className="signup-form" onSubmit={handleSubmit}>
+          <input
+            type="email"
+            value={email}
+            onChange={handleEmailChange}
+            placeholder="Enter your email"
+            required
+          />
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
